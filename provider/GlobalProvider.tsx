fc723aa5f7bcae82637b1cb7bb0046cd9b842490
putubhanuartha <putuaryabhanu03@gmail.com>
2023-10-30 10:49:45 +0700

    delete all

--- provider/GlobalProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-import { ThemeProvider } from "@material-tailwind/react";
-const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
-	return <ThemeProvider>{children}</ThemeProvider>;
-};
-
-export default GlobalProvider;
